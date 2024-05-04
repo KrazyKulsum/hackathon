03715296d2f95b439fcec08bbae29a232eea03c9
KrazyKulsum <94999119+KrazyKulsum@users.noreply.github.com>
2024-05-04 14:36:36 -0700

    heading, search button, about

---
+++ my-app/src/components/About.jsx
@@ -0,0 +1,12 @@
+// About.js
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <h1>About</h1>
+      <p>  Welcome to Halal Connect, your premier destination for discovering masjids and halal food places near you. Our platform is dedicated to simplifying your search for places of worship and dining options tailored to your needs. Whether you're traveling to a new city, relocating, or simply exploring your local area, Halal Connect provides a comprehensive solution. Easily locate nearby masjids  for daily prayers, Jumu'ah, and special events, or satisfy your cravings with our curated selection of halal food places, including restaurants, cafes, and markets. With our user-friendly interface, intuitive filters, and commitment to inclusivity and accessibility, finding what you need has never been easier. Join our vibrant community hub, where you can connect, share experiences, and stay updated on local events and initiatives. Whether you're a frequent traveler or a longtime member of the community, Halal Connect  is your go-to resource for all things related to the Muslim experience. Start exploring today and embark on a journey of spiritual fulfillment and culinary delights with us.      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ my-app/src/components/Heading.jsx
@@ -0,0 +1,15 @@
+// Heading.js
+
+import React from 'react';
+import './Heading.css';
+
+const Heading = () => {
+  return (
+    <div className="heading-container">
+      <h1 className="main-heading">Halal Connect</h1>
+      <h2 className="sub-heading">Find prayer spaces and halal food nearby - Your community your convenience</h2>
+    </div>
+  );
+}
+
+export default Heading;
